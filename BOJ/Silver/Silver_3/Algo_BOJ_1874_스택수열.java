b9aeb64523899919ac887cd1c4635b651148b8da
JEEHWAN KIM <72750840+ghwann@users.noreply.github.com>
2020-10-14 22:44:46 +0900

    28696 KB / 360 ms
    오랜만에 풀어본 스택 문제
    처음에는 ArrayList를 이용하여 +, - 값을 넣고 출력하는 형식으로 했는데 그 땐 2272 ms의 시간이 걸렸다.
    입출력에 어떤 형식을 쓰느냐에 따라 소요 시간의 차이가 극명함을 느낄 수 있었다.

---
+++ BOJ/Silver/Silver_3/Algo_BOJ_1874_스택수열.java
@@ -0,0 +1,41 @@
+package com.algorithm.study.BOJ.week09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Algo_BOJ_1874_스택수열 {
+	private static int N;
+	private static int[] input;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		N = Integer.parseInt(br.readLine());
+		input = new int[N+1];
+		Stack<Integer> s = new Stack<>();
+		
+		for (int i = 1; i <= N; i++) {
+			input[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int j = 1;
+		for (int i = 1; i <= N; i++) {
+			s.push(i);
+			sb.append("+").append('\n');
+			
+			while (!s.isEmpty() && s.peek() == input[j]) {
+				s.pop();
+				sb.append("-").append('\n');
+				j++;
+			}
+		}
+		
+		if (s.isEmpty()) {
+			System.out.println(sb);
+		} else {
+			System.out.println("NO");
+		}
+	}	// end of main
+}	// end of class
