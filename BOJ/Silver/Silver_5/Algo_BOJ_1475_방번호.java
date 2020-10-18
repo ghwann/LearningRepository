d9902562c2accdaf698f9bb2e933e0e9929e4658
JEEHWAN KIM <72750840+ghwann@users.noreply.github.com>
2020-10-18 23:59:22 +0900

    Update Algo_BOJ_1475_방번호.java

--- BOJ/Silver/Silver_5/Algo_BOJ_1475_방번호.java
+++ BOJ/Silver/Silver_5/Algo_BOJ_1475_방번호.java
@@ -10,6 +10,7 @@ public class Algo_BOJ_1475_방번호 {
 		String s = sc.next();
 		numbers = new int[10];
 		
+		// 6과 9는 바꿔쓸 쑤 있으므로 9일 경우엔 6으로 처리
 		for (int i = 0; i < s.length(); i++) {
 			int index = s.charAt(i) - '0';
 			numbers[index == 9 ? 6 : index]++;
@@ -18,6 +19,7 @@ public class Algo_BOJ_1475_방번호 {
 		int max = 0;
 		for (int i = 0; i < 9; i++) {
 			if (i == 6) {
+				// 6과 9가 한 세트에 포함되므로 2로 나눈 몫에 나머지가 0이 아닐 경우는 한 세트가 더 필요한 경우이므로 1을 더
 				numbers[i] = numbers[i] / 2 + (numbers[i] % 2 == 0 ? 0 : 1); 
 			}
 			
