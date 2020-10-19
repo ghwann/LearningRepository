c01a86925f2e8fa8853ffb2e5d37ad532ca3fe0f
JEEHWAN KIM <72750840+ghwann@users.noreply.github.com>
2020-10-19 23:40:08 +0900

    Update DBUtil.java

--- MVC2/car/src/com/ssafy/util/DBUtil.java
+++ MVC2/car/src/com/ssafy/util/DBUtil.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-	private static final String URL = "jdbc:mysql://127.0.0.1:3306/javaadd?serverTimezone=UTC";
+	private static final String URL = "jdbc:~";
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String ID = "ssafy";
-	private static final String PW = "1234";
+	private static final String ID = "test1";
+	private static final String PW = "****";
 	
 	static {
 		try {
