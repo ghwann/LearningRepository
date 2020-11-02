5a60da9754fa2d0c7b5cfa7a01919ae4690c0060
JEEHWAN KIM <72750840+ghwann@users.noreply.github.com>
2020-11-02 18:00:45 +0900

    Update Algo_BOJ_1926_그림.java

--- BOJ/Silver/Silver_1/Algo_BOJ_1926_그림.java
+++ BOJ/Silver/Silver_1/Algo_BOJ_1926_그림.java
@@ -1,65 +1,34 @@
-package com.algorithm.study.BOJ.week01;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Algo_BOJ_1926 {
-	private static int N, M, max_size, now_size, cnt;
-	private static int[][] canvas;
+public class Algo_BOJ_1926_그림 {
+	private static int N, M, cnt, now_size, max_size;
+	private static int[][] map;
 	private static boolean[][] visited;
-	private static int[] dr = {-1, 1, 0, 0};	// 상, 하, 좌, 우
-	private static int[] dc = {0, 0, -1, 1};
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		canvas = new int[N][M];
+		map = new int[N][M];
 		visited = new boolean[N][M];
 		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for (int j = 0; j < M; j++) {
-				canvas[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		Queue<Loc> q = new LinkedList<Loc>();
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (!visited[i][j] && canvas[i][j] == 1) {
-					Loc l = new Loc(i, j);
-					q.offer(l);
-					cnt++;
-					now_size = 1;
-					visited[i][j] = true;
-					
-					while (!q.isEmpty()) {
-						l = q.poll();
-						int r = l.r;
-						int c = l.c;
-						
-						for (int k = 0; k < dr.length; k++) {
-							int nr = r + dr[k];
-							int nc = c + dc[k];
-							
-							if (0 <= nr && nr < N && 0 <= nc && nc < M && !visited[nr][nc] 
-								&& canvas[nr][nc] == 1) {
-								now_size++;
-								q.offer(new Loc(nr, nc));
-								visited[nr][nc] = true;
-							}
-						}
-					}
-				}
-				if (now_size > max_size) {
-					max_size = now_size;
+				if (!visited[i][j] && map[i][j] == 1) {
+					bfs(i, j);
 				}
 			}
 		}
@@ -67,12 +36,47 @@ public class Algo_BOJ_1926 {
 		System.out.println(max_size);
 	}	// end of main
 
-	static class Loc {
-		int r, c;
-
-		public Loc(int r, int c) {
-			this.r = r;
-			this.c = c;
+	// 00 01 02
+	// 10 11 12
+	// 20 21 22
+	private static int[] dr = {-1, 1, 0, 0};	// 상, 하, 좌, 우
+	private static int[] dc = {0, 0, -1, 1};
+	private static void bfs(int r, int c) {
+		Queue<Pos> q = new LinkedList<Pos>();
+		Pos p = new Pos(r, c);
+		q.offer(p);
+		cnt++;
+		now_size = 1;
+		visited[r][c] = true;
+		
+		while (!q.isEmpty()) {
+			p = q.poll();
+			int x = p.x;
+			int y = p.y;
+			
+			for (int i = 0; i < dc.length; i++) {
+				int nr = x + dr[i];
+				int nc = y + dc[i];
+				
+				if (0 <= nr && nr < N && 0 <= nc && nc < M && !visited[nr][nc] && map[nr][nc] == 1) {
+					now_size++;
+					q.offer(new Pos(nr, nc));
+					visited[nr][nc] = true;
+				}
+			}
+		}
+		if (now_size > max_size) {
+			max_size = now_size;
+		}
+	}
+	
+	static class Pos {
+		int x;
+		int y;
+		
+		public Pos(int x, int y) {
+			this.x = x;
+			this.y = y;
 		}
 	}
 }	// end of class
