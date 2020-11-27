aa73e7910b3c9e50e03b0ad09d2c29b5a2393537
JEEHWAN KIM <72750840+ghwann@users.noreply.github.com>
2020-11-27 23:45:46 +0900

    풀이_두번째

--- BOJ/Silver/Silver_3/Algo_BOJ_1874_스택수열.java
+++ BOJ/Silver/Silver_3/Algo_BOJ_1874_스택수열.java
@@ -1,41 +1,44 @@
-package com.algorithm.study.BOJ.week09;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Algo_BOJ_1874_스택수열 {
-	private static int N;
-	private static int[] input;
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		N = Integer.parseInt(br.readLine());
-		input = new int[N+1];
-		Stack<Integer> s = new Stack<>();
-		
-		for (int i = 1; i <= N; i++) {
-			input[i] = Integer.parseInt(br.readLine());
-		}
-		
-		int j = 1;
-		for (int i = 1; i <= N; i++) {
-			s.push(i);
-			sb.append("+").append('\n');
-			
-			while (!s.isEmpty() && s.peek() == input[j]) {
-				s.pop();
-				sb.append("-").append('\n');
-				j++;
-			}
-		}
-		
-		if (s.isEmpty()) {
-			System.out.println(sb);
-		} else {
-			System.out.println("NO");
-		}
-	}	// end of main
-}	// end of class
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int i=1;
+        int cnt = 0;
+        int a = Integer.parseInt(br.readLine());
+        while(true) {
+            // stack이 비어있지 않다면
+            if(!stack.isEmpty()) {
+                // stack의 맨 위의 수와 다음 수열의 수가 같은지 확인
+                if(stack.peek() == a) {
+                    // 같다면 pop
+                    sb.append("-").append("\n");
+                    stack.pop();
+                    cnt++;
+                    // 수열을 모두 생성한 경우
+                    if(cnt == n)
+                        break;
+                    // 다음 수 입력받기
+                    a = Integer.parseInt(br.readLine());
+                    continue;
+                }                
+            }
+            stack.add(i++);
+            sb.append("+").append("\n");
+            // i가 n+1이 넘으면 수열을 생성할 수 없는 경우
+            if(i > n+1)
+                break;
+        }
+        if(stack.size() == 0)
+            System.out.print(sb);
+        else
+            System.out.println("NO");    // stack에 남아 있다면 수열을 생성하지 못함
+    }
+}
