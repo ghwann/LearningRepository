8bc3d3b0b895cf499ed9f1b73b87e0943d2e1d9e
JEEHWAN KIM <72750840+ghwann@users.noreply.github.com>
2020-11-29 23:58:50 +0900

    Update Algo_BOJ_1182_부분수열의합.java

--- BOJ/Silver/Silver_2/Algo_BOJ_1182_부분수열의합.java
+++ BOJ/Silver/Silver_2/Algo_BOJ_1182_부분수열의합.java
@@ -1,44 +1,51 @@
-package com.algorithm.study.BOJ.week06;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
-public class Algo_BOJ_1182_부분수열의합 {
-	private static int N, S, cnt;
-	private static int[] input;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		S = Integer.parseInt(st.nextToken());
-		input = new int[N];
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		powerSet(input, N, 0, 0);
-		
-		if (S == 0) {
-			cnt--;
-		}
-		
-		System.out.println(cnt);
-	}	// end of main
+/**
+ * BOJ#1182 부분집합의 합
+ * https://www.acmicpc.net/problem/1182
+ */
 
-	private static void powerSet(int[] input, int n, int index, int sum) {
-		if (index == n) {
-			if (sum == S) {
-				cnt++;
-			}
-			return;
-		}
-		
-        powerSet(input, n, index + 1, sum);
-        powerSet(input, n, index + 1, sum + input[index]);
-	}
-}	// end of class
+public class Main {
+
+    static int N;
+    static int S;
+    static int cnt = 0;
+    static int[] numArr = new int[21];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+
+            numArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        solve(0, 0);
+
+        // S가 0인 경우 공집합도 포함되므로 개수에서 -1을 빼준다.
+        if (S == 0) {
+            cnt -= 1;
+        }
+
+        System.out.println(cnt);
+    }
+
+    static void solve(int sum, int step) {
+
+        if (step == N) {
+
+            if (sum == S) cnt++;
+            return;
+        }
+
+        solve(sum + numArr[step], step + 1);
+        solve(sum, step + 1);
+    }
+}
